94db3d64d007d072f4169064300e3e5899af1c52
RJ993 <196080894+RJ993@users.noreply.github.com>
2026-06-08 15:32:24 -0400

    Add way to navigate through books and start on new book

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const myLibrary = [];
+const myLibrary = [new Book("Love", "Shrek"), new Book("Chess", "John Chess")];
+let bookPointer = 0;
+const backButton = document.querySelector(".back");
+const forwardButton = document.querySelector(".forward");
+const titleDis = document.querySelector("#title");
+const authorDis = document.querySelector("#author");
+
 
 function Book(title, author) {
   if (!new.target) {
@@ -10,6 +16,37 @@ function Book(title, author) {
 }
 
 function addBookToLibrary(title, author) {
-  const newBook = new Book(title, author)
-  myLibrary.push(newBook)
+  const newBook = new Book(title, author);
+  myLibrary.push(newBook);
 }
+
+function goBack(){
+  if (bookPointer == 0){
+    bookPointer = myLibrary.length - 1;
+  }else{
+    bookPointer--;
+  }
+}
+
+function goForward(){
+  if (bookPointer == myLibrary.length - 1){
+    bookPointer = 0;
+  }else{
+    bookPointer++;
+  }
+}
+
+function changeContent(){
+  titleDis.textContent = myLibrary.at(bookPointer).title;
+  authorDis.textContent = myLibrary.at(bookPointer).author;
+}
+
+backButton.addEventListener('click', (e) => {
+  goBack();
+  changeContent();
+});
+
+forwardButton.addEventListener('click', (e) => {
+  goForward();
+  changeContent();
+});
